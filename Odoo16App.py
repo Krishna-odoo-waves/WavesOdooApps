75db2fbd5460724dca35f39fc7c6a15bb50e9366
Krishna <odoowaves.solution@gmail.com>
2023-12-05 21:13:24 +0530

    [ADD] 16 script

---
+++ Odoo16App.py
@@ -0,0 +1,7 @@
+MODULES16 = {
+    'odoo_python_pip_install_library':{
+        'name':'Odoo Python Pip Installer',
+        'url':'git@github.com:Krishna-odoo-waves/odoo_python_pip_install_library.git',
+        'depends':[],
+    }
+}
